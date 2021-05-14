110a36a0c1744f03c77b6e78866043af7a012574
Mondain - Monval <thoval@ceh.ac.uk>
2021-05-14 11:07:22 +0100

    Intial commit of file to get a single raster of the decide score across the whole GB

---
+++ scripts/functions/lotus_decide_score.R
@@ -0,0 +1,33 @@
+
+
+## Lotus create decide score
+library(rslurm)
+
+
+# taxa
+taxa = 'moth'
+
+decide_score <- function(index){
+  
+  
+  taxa = file_for_lotus$taxa
+  
+  ## load in species
+  # get names
+  
+  
+  # predictions
+  
+  # variation
+  
+  ## stack them together to get two rasterstacks
+  
+  ## combine them together to create decide score
+  
+  # preds*sqrt(occ)
+  
+  ## write single raster 
+  
+  
+  
+}
