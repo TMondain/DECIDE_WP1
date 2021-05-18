8229611d0915df1fc51e94aef36ea77d611c12a4
Mondain - Monval <thoval@ceh.ac.uk>
2021-05-18 16:35:28 +0100

    made changes to be able to create weighted averages across species based on the probability of presence layers, chosen by the new 'comb_method' parameter.

--- scripts/functions/lotus_decide_score.R
+++ scripts/functions/lotus_decide_score.R
@@ -1,4 +1,4 @@
-
+setwd("~/DECIDE/DECIDE_WP1")
 
 ## Lotus create decide score
 library(rslurm)
@@ -11,21 +11,34 @@ pars = data.frame(index = 1:2)
 # pseudoabsence name (i.e. the model)
 pseudoabs_name = 'PA_thinned_10000nAbs'
 
-file_for_lotus <- data.frame(taxa = c('moth', 'butterfly'),
-                             pseudoabs_name = rep('PA_thinned_10000nAbs', 2))
+# decide score method
+method = 'var_sqroute_preds'
+
+# combine decide score method
+comb_method = 'weight_mean'
+
+# main directory
+main_dir = '/gws/nopw/j04/ceh_generic/thoval/DECIDE/SDMs/outputs/'
 
 decide_score <- function(index){
+  
+  require(raster)
   
   # read in file
   file_for_lotus <- read.csv('file_for_lotus.csv')
   
+  # route directory
+  main_dir = file_for_lotus$main_dir[index]
+  
   # get the parameters set
   taxa = file_for_lotus$taxa[index]
-  preudoabs_name = file_for_lotus$preudoabs_name[index]
+  pseudoabs_name = file_for_lotus$pseudoabs_name[index]
+  method = file_for_lotus$method[index]
+  comb_method = file_for_lotus$comb_method[index]
   
   ## load in species
   # get names
-  fls <- paste0('/gws/nopw/j04/ceh_generic/thoval/DECIDE/SDMs/outputs/', taxa,'/combined_model_outputs/',pseudoabs_name)
+  fls <- paste0(main_dir, taxa,'/combined_model_outputs/',pseudoabs_name)
   
   # predictions
   preds <- raster::stack(list.files(fls, 
@@ -39,13 +52,48 @@ decide_score <- function(index){
   
   
   ## combine them together to create decide score
-  dec_spp <- preds*sqrt(var)
+  if(method == 'var_sqroute_preds'){
+    dec_spp <- preds*sqrt(var)
+  } else { stop(paste('!!!   script only coded for methods var_sqroute_preds   !!!')) }
   
   # get mean across all species
-  decide <- calc(dec_spp, mean)
+  if(comb_method == 'mean'){
+    decide <- calc(dec_spp, mean)
+  } else if(comb_method == 'weight_mean'){
+    decide <- raster::weighted.mean(dec_spp, preds)
+  } else { stop(paste('!!!   script only coded for combining methods mean and weight_mean   !!!')) }
+  
   
   ## write single raster 
-  
-  
+  writeRaster(x = decide, 
+              filename = paste0(main_dir, 'decide_scores/outputs/', taxa, '_',  pseudoabs_name, '_decide_score_', method, '_', comb_method, '.grd'),
+              format = 'raster', overwrite = T)
   
 }
+
+
+#####     The script to create the job submission files
+setwd(paste0('scripts/lotus/decide_scores/'))
+
+sdm_slurm <- slurm_apply(decide_score,
+                         params = pars,
+                         jobname = paste0("decide_scores_", pseudoabs_name, "_", method, "_", comb_method),
+                         nodes = length(unique(pars$index)),
+                         cpus_per_node = 1,
+                         slurm_options = list(partition = "short-serial",
+                                              time = "23:59:59",
+                                              mem = "30000"),
+                                              # error = paste0(main_dir,'decide_scores/', '_rslurm_decide_scores_', pseudoabs_name, '_', method, '/', method,'_error-%j-%a.out')),
+                         rscript_path = '',
+                         submit = F)
+
+
+# create file for lotus
+file_for_lotus <- data.frame(taxa = c('moth', 'butterfly'),
+                             pseudoabs_name = rep(pseudoabs_name, 2),
+                             method = rep(method, 2), 
+                             main_dir = rep(main_dir, 2),
+                             comb_method = rep(comb_method, 2))
+
+write.csv(file_for_lotus, 
+          file = paste0('_rslurm_decide_scores_', pseudoabs_name, '_', method,'_', comb_method, '/file_for_lotus.csv'))
